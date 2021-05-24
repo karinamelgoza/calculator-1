f21d903f95d1d8086132170b78a0bb28cc7e2b49
Karina Melgoza <kmelgoza08@gmail.com>
2021-05-24 15:40:25 -0700

    further study

---
+++ calculator/calc.js
@@ -0,0 +1,53 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function (input) {
+	tokens = input.split(' ');
+
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+	console.log('num3', num3);
+
+	if (mathSymbol === '+') {
+		console.log(num1 + num2 + num3);
+	}
+	if (mathSymbol === '-') {
+		console.log(num1 - num2 - num3);
+	}
+	if (mathSymbol === '*') {
+		console.log(num1 * num2 * num3);
+	}
+	if (mathSymbol === '/') {
+		console.log(num1 / num2 / num3);
+	}
+	if (mathSymbol === 'sqrt') {
+		console.log(Math.sqrt(num1));
+	}
+	if (mathSymbol === 'cube') {
+		console.log(Math.cbrt(num1));
+	}
+	if (mathSymbol === 'power') {
+		console.log(Math.pow(num1, num2));
+	}
+	if (mathSymbol === 'modulus') {
+		console.log(num1 % num2);
+	}
+
+
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
